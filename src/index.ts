dd62f4130d3df9c07eb1339827a7d7a0fd0c0c17
ourongxing <orongxing@gmail.com>
2025-04-28 16:03:31 +0800

    feat: support custom base url

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+import { env } from "node:process"
 import { FastMCP } from "fastmcp"
 import { z } from "zod"
 import { $fetch } from "ofetch"
+import { config } from "dotenv"
+import packageJson from "../package.json"
 import { description } from "./process"
 import type { SourceResponse } from "./typing"
 
+config()
+
 const server = new FastMCP({
   name: "NewsNow",
-  version: "0.0.1",
+  version: packageJson.version as `${number}.${number}.${number}`,
 })
 
-console.log(description)
+console.log(env.BASE_URL)
 
 server.addTool({
   name: `get_hotest_latest_news`,
@@ -18,8 +23,13 @@ server.addTool({
     id: z.string(),
     count: z.number().default(10),
   }),
+  annotations: {
+    title: "get news from hotest or latest, by id",
+    readOnlyHint: true,
+  },
   execute: async ({ id, count }) => {
-    const res: SourceResponse = await $fetch(`https://newsnow.busiyi.world/api/s?id=${id}`)
+    const baseUrl = env.BASE_URL ?? "https://newsnow.busiyi.world"
+    const res: SourceResponse = await $fetch(`${baseUrl}/api/s?id=${id}`)
     return res.items.slice(0, count).map((item) => {
       return `[${item.title}](${item.url})`
     }).join("\n")
