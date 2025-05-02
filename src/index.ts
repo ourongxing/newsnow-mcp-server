92754fc474a3b468cf1fffb2df33bbfb499c88a2
Ou <orongxing@gmail.com>
2025-05-02 19:54:32 +0800

    feat: better prompt

--- src/index.ts
+++ src/index.ts
@@ -22,21 +22,24 @@ const server = new FastMCP({
 })
 
 server.addTool({
-  name: `get_hotest_latest_news`,
-  description: `get hotest or latest news from source by id. ${description}`,
+  name: "get_hotest_latest_news",
+  description: `get hotest or latest news from source by {id}, return {count: 10} news.`,
   parameters: z.object({
-    id: z.string(),
-    count: z.number().default(10),
+    id: z.string().describe(`source id. e.g. ${description}`),
+    count: z.any().default(10).describe("count of news to return."),
   }),
-  annotations: {
-    title: "get hotest or latest news from source by id.",
-    readOnlyHint: true,
-  },
   execute: async ({ id, count }) => {
+    let n = Number(count)
+    if (Number.isNaN(n) || n < 1) {
+      n = 10
+    }
     const res: SourceResponse = await $fetch(`${baseUrl}/api/s?id=${id}`)
-    return res.items.slice(0, count).map((item) => {
-      return `[${item.title}](${item.url})`
-    }).join("\n")
+    return {
+      content: res.items.slice(0, count).map(item => ({
+        text: `[${item.title}](${item.url})`,
+        type: "text",
+      })),
+    }
   },
 })
 
